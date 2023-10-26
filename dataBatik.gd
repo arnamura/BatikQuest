c7d67cdd4081e4809b69f9cb7b2bcc683e064278
Arna Muhammad Rasyid <72067766+arnamura@users.noreply.github.com>
2023-10-26 17:39:25 +0700

    nambah file dataBatik untuk database batik edit dikit museum

---
+++ dataBatik.gd
@@ -0,0 +1,64 @@
+extends Resource
+
+var batik1 := {
+	"nama": "Ceplok",
+	"pathimg": "res://art/batik/ceplok.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
+
+var batik2 := {
+	"nama": "Gringsing",
+	"pathimg": "res://art/batik/gringsing.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
+
+var batik3 := {
+	"nama": "kawung",
+	"pathimg": "res://art/batik/kawung.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
+
+var batik4 := {
+	"nama": "Parang",
+	"pathimg": "res://art/batik/parang.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
+
+var batik5 := {
+	"nama": "Pring Sedapur",
+	"pathimg": "res://art/batik/pringSedapur.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
+
+var batik6 := {
+	"nama": "Sawat",
+	"pathimg": "res://art/batik/sawat.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
+
+var batik7 := {
+	"nama": "Sekar Jagad",
+	"pathimg": "res://art/batik/sekarjagad.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
+
+var batik8 := {
+	"nama": "Sidomukti",
+	"pathimg": "res://art/batik/sidomukti.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
+
+var batik9 := {
+	"nama": "truntum",
+	"pathimg": "res://art/batik/truntum.png",
+	"deskripsi" : "tba",
+	"isGet": false
+}
