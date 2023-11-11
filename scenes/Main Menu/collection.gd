798c2fa105bf8182e5f134be1f426a14b3a3cd8a
Arna Muhammad Rasyid <72067766+arnamura@users.noreply.github.com>
2023-11-11 19:26:19 +0700

    edit dikit bagian museum update collectible dengan slot

---
+++ scenes/Main Menu/collection.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_btn_ext_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main Menu/main_menu.tscn")
---
+++ scenes/Main Menu/main_menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+
+func _on_btn_new_g_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_load_g_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_sett_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_col_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main Menu/collection.tscn")
+
+
+func _on_btn_ext_pressed():
+	get_tree().quit()
