60fc12c2c67a6c2110068008dfcab011f1b6c89b
Arna Muhammad Rasyid <72067766+arnamura@users.noreply.github.com>
2023-12-22 16:45:40 +0700

    memelengkapi beberapa deskripsi dan ciri batik

--- dataBatik.gd
+++ dataBatik.gd
@@ -6,9 +6,9 @@ var visibleDesc = false
 var batik1 := {
 	"nama": "Ceplok",
 	"pathimg": "res://art/batik/ceplok.png",
-	"deskripsi" : 
+	"deskripsi" :
 		"Motif batik yang berasal dari kalangan petani dan rakyat. Inspirasinya berasal dari bunga-bunga yang tumbuh di semak-semak di pedesaan. Tidak ada filosofi khusus. Namun, dari segi geometri, motif ini dapat mencerminkan keseimbangan, keteraturan, dan kesempurnaan bagi mereka yang memakainya.",
-	"ciri" : 
+	"ciri" :
 		"Motif kelompok bunga-bunga yang dihias dengan daun disekelilingnya menjadi kerangka segi empat\n(Ceplokan: Didasari bentuk-bentuk geometris persegi/oval/bintang yang disusun melingkar menjadi sekuntum bunga)",
 	"isGet": false
 }
@@ -16,24 +16,24 @@ var batik1 := {
 var batik2 := {
 	"nama": "Gringsing",
 	"pathimg": "res://art/batik/gringsing.png",
-	"deskripsi" : "tba",
-	"ciri" : "batik",
+	"deskripsi" : "Kain ini dipakai sebagai hiasan pada kereta megah yang telah ada sejak zaman Raden Wijaya di Majapahit. Kain ini juga merupakah hadiah untuk panglima perang yang telah berjasa. Konon, kain ini memiliki kekuatan untuk menyembuhkan luka",
+	"ciri" : "Memiliki motif lingkaran kecil-kecil sebagai latar dari kain dengan sebuah titik di tengah lingkaran tersebut",
 	"isGet": false
 }
 
 var batik3 := {
 	"nama": "kawung",
 	"pathimg": "res://art/batik/kawung.png",
-	"deskripsi" : "tba",
-	"ciri" : "batik",
+	"deskripsi" : "Diciptakan oleh Sultan Mataran, yaitu Sultan Agung Hanyokrokusumo. Motif Batik ini merupakan larangan di Keraton Yogyakarta yang hanya boleh dipakai oleh Raja dan Sentono dalem(Keluarga Raja)",
+	"ciri" : "Berbentuk seperti 4 buah kolang-kaling yang disusun menjadi lingkaran dan terdapat ornamen kecil ditengahnya",
 	"isGet": true
 }
 
 var batik4 := {
 	"nama": "Parang",
 	"pathimg": "res://art/batik/parang.png",
-	"deskripsi" : "tba",
-	"ciri" : "batik",
+	"deskripsi" : "Ikon Batik Indonesia yang mudah dikenali. Diciptkan oleh Panembahan Senopati. Dipakai oleh Raja, Pemaisuri dan Putra Putri Keraton Surakarta yang merupakan 'ageman luhur' ",
+	"ciri" : "Berasal dari kata 'pereng' yang berati lereng atau pinggiran tebing yang miring berbentuk diagonal. Terinspirasi dari meditasi penambahan senopati yang sedang memperhatikan gejolak ombak pantai selatan yang sedang menghantam batuk karang",
 	"isGet": false
 }
 
